63af12f52951ed8559680ce6385e3b1a2a039497
Dean Chapman <dean.chapman@kroo.com>
2025-09-20 00:08:54 +0100

    Add more request tests

--- test/vertx_ring/adapter_test.clj
+++ test/vertx_ring/adapter_test.clj
@@ -21,24 +21,49 @@
 
       (is true "Server started and stopped successfully"))))
 
+(def default-request
+  {:body nil
+   :protocol "HTTP/1.1",
+   :remote-addr "127.0.0.1",
+   :request-method :get,
+   :server-name "localhost",
+   :server-port 8080
+   :headers {},
+   :query-string nil,
+   :scheme :http
+   :uri "/"})
+
 (deftest test-request-mapping
   (testing "Request mapping from Vert.x to Ring:"
     (testing "defaults"
-      (is (= {:body nil
-              :headers {"simple" "Value"
-                        "cookie" "cookie1=value1; cookie2=value2"}
-              :protocol "HTTP/1.1",
-              :query-string "a=b&c=d",
-              :remote-addr "127.0.0.1",
-              :request-method :get,
-              :scheme :https,
-              :server-name "localhost",
-              :server-port 8080,
-              :uri "/hello"}
-             (-> {:query "a=b&c=d"
-                  :uri "/hello"
-                  :scheme "https"
-                  :headers {"Simple" "Value"}
+      (is (= default-request
+             (-> {}
+                 (helpers/mock-http-server-request)
+                 (adapter/request->ring-map)))))
+
+    (testing "query"
+      (is (= (assoc default-request  :query-string "a=b&c=d")
+             (-> {:query "a=b&c=d"}
+                 (helpers/mock-http-server-request)
+                 (adapter/request->ring-map)))))
+
+    (testing "uri"
+      (is (= (assoc default-request :uri "/hello")
+             (-> {:uri "/hello"}
+                 (helpers/mock-http-server-request)
+                 (adapter/request->ring-map)))))
+
+    (testing "scheme"
+      (is (= (assoc default-request :scheme :https)
+             (-> {:scheme "https"}
+                 (helpers/mock-http-server-request)
+                 (adapter/request->ring-map)))))
+
+    (testing "headers"
+      (is (= (assoc default-request
+                    :headers {"simple" "Value"
+                              "cookie" "cookie1=value1; cookie2=value2"})
+             (-> {:headers {"Simple" "Value"}
                   :cookies {"cookie1" "value1" "cookie2" "value2"}}
                  (helpers/mock-http-server-request)
                  (adapter/request->ring-map)))))))
